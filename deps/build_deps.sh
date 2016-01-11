2f763af3e051825ed8a396fdcca695d73adfdb38
John Hamelink <john@johnhamelink.com>
2016-01-11 20:43:12 +0000

    Cleanup build_deps.sh file, move to deps

--- deps/build_deps.sh
+++ deps/build_deps.sh
@@ -5,42 +5,63 @@ KERNEL=$(echo $(lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | h
 LIB_PHONE_NUMBER_REPO=$1
 LIB_PHONE_NUMBER_REV=$2
 
-echo "Use repo ${LIBPHONE_NUMBER_REPO} and revision ${LIBPHONE_NUMBER_REV}"
+log()
+{
+  echo ":: $1"
+}
+
+error()
+{
+  echo "[ERROR] $1"
+  exit 1
+}
 
 install_libphonenumber()
 {
+  log "Cloning Libphonenumber"
   git clone ${LIB_PHONE_NUMBER_REPO}
-  pushd libphonenumber
+  pushd libphonenumber > /dev/null
   git checkout ${LIB_PHONE_NUMBER_REV}
-  popd
+  popd > /dev/null
 
   mkdir -p libphonenumber/cpp/build
-  pushd libphonenumber/cpp/build
+  pushd libphonenumber/cpp/build > /dev/null
   export CFLAGS=-fPIC
   export CXXFLAGS=-fPIC
+  log "Building Libphonenumber"
   cmake -DCMAKE_INSTALL_PREFIX:PATH=install  ..
   make
   make install
-  popd
+  popd > /dev/null
 }
 
 check_for_package()
 {
+  log "Checking for $1"
   if [ "`apt-cache policy $1 | grep "(none)"`" ]; then
-    echo "Error: Cannot find $1 - install it with apt-get install $1"
+    error "Cannot find $1 - install it with apt-get install $1"
     exit 1;
+  else
+    log "Found $1"
   fi
+
+  log ""
 }
 
+log "Use repo ${LIB_PHONE_NUMBER_REPO} and revision ${LIB_PHONE_NUMBER_REV}"
+
 mkdir -p deps
-pushd deps
+pushd deps > /dev/null
+
+log "Detecting OS"
 
 case $OS in
   Linux)
     case $KERNEL in
       Ubuntu)
-        echo "Linux, Ubuntu"
+        log "Linux, Ubuntu"
 
+        check_for_package "git"
         check_for_package "cmake"
         check_for_package "libprotobuf-dev"
         check_for_package "protobuf-compiler"
@@ -54,10 +75,10 @@ case $OS in
 
         install_libphonenumber
         ;;
-      *) echo "Your system $KERNEL is not supported"
+      *) error "Your system $KERNEL is not supported"
     esac
     ;;
-  *) echo "Your system $OS is not supported"
+  *) error "Your system $OS is not supported"
 esac
 
 popd
