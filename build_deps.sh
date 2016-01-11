7c94da035b7e2c8e1db041b7fe9a276c3cad372b
John Hamelink <john@johnhamelink.com>
2016-01-11 20:10:54 +0000

    Remove apt-get install from build_deps.sh
    This is something that should be done outside of a dependency
    installation.

--- build_deps.sh
+++ build_deps.sh
@@ -7,26 +7,29 @@ LIB_PHONE_NUMBER_REV=$2
 
 echo "Use repo ${LIBPHONE_NUMBER_REPO} and revision ${LIBPHONE_NUMBER_REV}"
 
-##echo $OS
-##echo $KERNEL
-
 install_libphonenumber()
 {
-	git clone ${LIB_PHONE_NUMBER_REPO}
-	pushd libphonenumber
-	git checkout ${LIB_PHONE_NUMBER_REV}
-	popd
+  git clone ${LIB_PHONE_NUMBER_REPO}
+  pushd libphonenumber
+  git checkout ${LIB_PHONE_NUMBER_REV}
+  popd
 
-	mkdir -p libphonenumber/cpp/build
-	pushd libphonenumber/cpp/build
-	export CFLAGS=-fPIC
-    export CXXFLAGS=-fPIC
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=install  ..
-	make
-	make install
-	#mkdir -p ../../../../priv
-	#cp install/lib/*.so ../../../../priv/
-	popd
+  mkdir -p libphonenumber/cpp/build
+  pushd libphonenumber/cpp/build
+  export CFLAGS=-fPIC
+  export CXXFLAGS=-fPIC
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=install  ..
+  make
+  make install
+  popd
+}
+
+check_for_package()
+{
+  if [ "`apt-cache policy $1 | grep "(none)"`" ]; then
+    echo "Error: Cannot find $1 - install it with apt-get install $1"
+    return 1;
+  fi
 }
 
 mkdir -p deps
@@ -34,24 +37,26 @@ pushd deps
 
 case $OS in
   Linux)
-     case $KERNEL in
-       CentOS)
-          echo "Linux, CentOS not supported yet"
-          ;;
-       Ubuntu)
-         echo "Linux, Ubuntu"
-			wget http://mt.archive.ubuntu.com/ubuntu/pool/universe/r/re2/libre2-1_20140304+dfsg-2_amd64.deb -O libre2-1.deb
-			wget http://es.archive.ubuntu.com/ubuntu/pool/universe/r/re2/libre2-dev_20140304+dfsg-2_amd64.deb -O libre2-dev.deb
-			sudo dpkg -i libre2*.deb
-			sudo apt-get install cmake cmake-curses-gui libprotobuf-dev libgtest-dev libre2-dev libicu-dev libboost-dev libboost-thread-dev libboost-system-dev protobuf-compiler
-			install_libphonenumber
-         ;;
-       *) echo "Your system $KERNEL is not supported"
-     esac
-     ;;
-  Darwin)
-     echo "Mac OS not supported yet"
-     ;;
+    case $KERNEL in
+      Ubuntu)
+        echo "Linux, Ubuntu"
+
+        check_for_package "cmake"
+        check_for_package "libprotobuf-dev"
+        check_for_package "protobuf-compiler"
+        check_for_package "libgtest-dev"
+        check_for_package "libre2-1"
+        check_for_package "libre2-dev"
+        check_for_package "libicu-dev"
+        check_for_package "libboost-dev"
+        check_for_package "libboost-thread-dev"
+        check_for_package "libboost-system-dev"
+
+        install_libphonenumber
+        ;;
+      *) echo "Your system $KERNEL is not supported"
+    esac
+    ;;
   *) echo "Your system $OS is not supported"
 esac
 
